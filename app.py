be8bd739f29fff45bdbd48f816ac60c3454ad8f6
pedromarsan06 <pedromarsan06@gmail.com>
2025-03-26 19:39:06 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, app
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return "SEJA BEM VINDO AO APP EM PYTHON"
+@app.route("/repos")
+def repos():
+    return "Meus repos: https://www.github.com/pedromarsan06"
+if __name__ == "__main__":
+    app.run()
