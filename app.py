956a9350f168aff89177b71573a597ef67bd1c32
pedromarsan06 <pedromarsan06@gmail.com>
2025-03-26 20:07:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def homepage():
-    return "SEJA BEM VINDO AO APP EM PYTHON"
-@app.route("/repos")
-def repos():
-    return "Meus repos: https://www.github.com/pedromarsan06"
+import routes
+
 if __name__ == "__main__":
     app.run()
